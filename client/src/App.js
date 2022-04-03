1d520dbba702f8cea6c20d3bd1db72db219d5fe1
Sergio Pedro <srosaped@outlook.com>
2022-04-03 12:58:37 +0100

    [tests] interaction between FE and BE

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,30 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [backendData, setBackendData] = useState([{}]);
+
+  useEffect(() => {
+    fetch("api").then(
+      response => response.json()
+    ).then(
+      data => {
+        setBackendData(data)
+      }
+    )
+  }, []);
+
   return (
     <div className="container">
+
+      {(typeof backendData.users === 'undefined') ? (
+        <p>Loading...</p>
+      ) : (
+        backendData.users.map((user, i) => (
+          <p key={i}>{user}</p>
+        ))
+      )}
       
     </div>
   );
