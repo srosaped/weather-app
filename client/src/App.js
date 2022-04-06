23c5ba4468cd767e487acb2060d2cecb3ea992ee
Sergio Pedro <srosaped@outlook.com>
2022-04-06 17:54:37 +0100

    [refactor] some FE code refactor

--- client/src/App.js
+++ client/src/App.js
@@ -1,83 +1,30 @@
 import React, { useState } from 'react';
-
-import axios from 'axios';
-
-import Header from './components/Header/Header';
-import Form from 'react-bootstrap/Form';
-
 import './App.css';
 import './components/Login/login.css';
+
+import Header from './components/Header/Header';
 import LoginBtn from './components/Login/LoginBtn';
 import LogoutBtn from './components/Login/LogoutBtn';
 import Profile from './components/Profile/Profile';
+import { useAuth0 } from '@auth0/auth0-react';
+import Weather from './components/Weather/Weather';
 
 function App() {
 
-  const [weatherData, setWeatherData] = useState([{}]);
-  const [city, setCity] = useState('');
-  const apiKey = process.env.REACT_APP_WEATHER_API_KEY;
-  const url= process.env.REACT_APP_WEATHER_API_URL;
+  const { isLoading } = useAuth0();
 
-  const url_api =`${url}weather?q=${city}&appid=${apiKey}`;
-
-  const getWeather = () => {
-    axios.get(url_api).then(res => { 
-      setWeatherData(res.data)
-      console.log(res.data)
-    }).catch(err => {
-      console.log(err)
-    })
-  }
-
+  if (isLoading) return <div>Loading...</div>
 
   return (
   
     <div className="w-container">
       <div className='login-wrapper'>
-        <Profile />
-        <LoginBtn />
+        <Profile />  
         <LogoutBtn />
       </div>
-      <Header />
-          <Form.Select
-            className='select-city'
-            onChange={(e) => {
-              const selectedCity = e.target.value;
-              setCity(selectedCity);
-            }}
-            onClick={getWeather}
-          >
-            <option value='null'>Choose your city...</option>
-            <option value="Lisbon">Lisbon</option>
-            <option value="Leiria">Leiria</option>
-            <option value="Coimbra">Coimbra</option>
-            <option value="Porto">Porto</option>
-            <option value="Faro">Faro</option>
-          </Form.Select>
-          
-          {typeof weatherData.main === 'undefined' ? (
-            <div>
-              <h2 className='sub-title'>Please choose your city wisely!</h2>
-            </div>
-          ) : (
-          
-          <div className="weather-widget">
-            <p className='w-name'>{weatherData.name}, PT</p>
-            <p className='w-date'>{new Date().toLocaleDateString()}</p>
-            <p className='w-temp'>{Math.round(weatherData.main.temp)}ºF</p>
-            <div className='w-description'>
-              <img alt='icon' className='w-icon' src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}/>
-              <p className='w-sky'>{weatherData.weather[0].description}</p>
-            </div>
-          </div>
-          )}
-
-          {weatherData.cod === "404" ? (
-            <p className='err-404'>Please choose a city from the dropdown</p>
-          ): (
-            <>
-            </>
-          )}
+        <LoginBtn />
+        <Header />
+        <Weather />
     </div>
   );
 }
